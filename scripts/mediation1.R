944644f58cbc054346b33b40533403106a84a6ec
Joseph Notis <joseph.a.notis.21@dartmouth.edu>
2021-02-26 17:18:23 -0500

    mediation script - a couple errors but mostly done

--- scripts/mediation1.R
+++ scripts/mediation1.R
@@ -8,50 +8,47 @@ library(lubridate)
 library(mediation)
 library(parallel)
 
-
-# Set up parallel computing -----------------------------------------------
-
-parallel::mclapply()
-
 # Load/wrangle data -------------------------------------------------------
 # https://stackoverflow.com/questions/49910861/removing-comma-from-numbers-in-r
 replaceCommas<-function(x){
   x<-as.numeric(gsub("\\,", "", x))
 }
 
-dta <- read.csv("data/combined_data.csv") 
+dta <- read.csv("data/combined_data.csv") %>% 
+  filter(month(date) == 4) # downsample to run some tests
 
-dta2 <- dta# %>% 
-  # mutate(state = factor(state),
-  #        county = factor(county),
-  #        margin = as.factor(margin),
-  #        mobility_label = factor(mobility_label)) %>%
-  # filter(month(date) == 4) #%>%  # downsample to run some tests
-  # mutate(date = factor(date))
-
-dta$popest_2019 <- sapply(dta2$popest_2019, replaceCommas)
+dta$popest_2019 <- sapply(dta$popest_2019, replaceCommas)
 
 
-dtalist <- dta2 %>% 
-  mutate(fact_state = factor(state)) %>% 
-  group_split(fact_state)
+dtalist <- dta %>% 
+  mutate(state = factor(state),) %>% 
+  group_split(state)
 
 # Run models --------------------------------------------------------------
 
-medfit <- function(dataset) {
-  med.fit <- lm(margin ~ percent_change_from_baseline +
-                  cases + 
+mediation_analysis <- function(dataset) {
+  med.fit <- lm(margin ~ cases + 
+                  involuntary +
                   date + 
-                  # state + 
                   county +
-                  mobility_label +
                   lockdowns +
                   popest_2019, 
                 data = dataset)
-  summary(med.fit)
+  
+  out.fit <- lm(voluntary ~ cases +
+                  margin +
+                  involuntary +
+                  date + 
+                  county +
+                  lockdowns +
+                  popest_2019, 
+                data = dataset)
+  
+  med.out <- mediate(med.fit, out.fit, treat = "cases", mediator = "margin", sims = 1000, boot = TRUE)
+  summary(med.out)
 }
 
-rslt <- mclapply(dtalist, medfit, mc.cores = 3)
+rslt <- mclapply(dtalist2, mediation_analysis, mc.cores = 3)
 
 # Run non-parallel
-# medfit(dta)
+# mediation_analysis(dta)
